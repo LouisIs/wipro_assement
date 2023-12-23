ea822b75bbe97bd0147ddb5db5c33f38674441b1
traore louis yvan <louisyvanisrael@gmail.com>
2023-12-23 02:05:47 -0500

    wipro assesment stage base

---
+++ wipro_assement/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace wipro_assement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
